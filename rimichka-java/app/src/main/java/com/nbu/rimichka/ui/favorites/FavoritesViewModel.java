ba5d1bc78a3333dae32d134015d77bf7a20c1611
Alexander Karaatanasov <akaraatanasov@gmail.com>
2020-01-12 21:38:03 +0200

    Initial project with navigation and icons

---
+++ rimichka-java/app/src/main/java/com/nbu/rimichka/ui/favorites/FavoritesViewModel.java
@@ -0,0 +1,19 @@
+package com.nbu.rimichka.ui.favorites;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class FavoritesViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public FavoritesViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
